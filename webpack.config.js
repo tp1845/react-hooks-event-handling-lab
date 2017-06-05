acb98480a8729c5b7bd18eb12f11ab45b96697f0
manyshyn <manyshynyuri@gmail.com>
2017-06-05 12:36:19 -0400

    Update starter code to use Webpack config

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
+
+// env
+const buildDirectory = './dist/';
+var APP_DIR = path.resolve(__dirname, 'src/');
+module.exports = {
+  entry: APP_DIR + '/index.js',
+  devServer: {
+    hot: true,
+    inline: true,
+    port: 7700,
+    historyApiFallback: true,
+  },
+  resolve: {
+    extensions: ['', '.js', '.jsx'],
+  },
+  output: {
+    path: path.resolve(buildDirectory),
+    filename: 'app.js',
+    publicPath: 'http://localhost:7700/dist',
+  },
+  externals: {
+    'cheerio': 'window',
+    'react/lib/ExecutionEnvironment': true,
+    'react/lib/ReactContext': true,
+  },
+  module: {
+    loaders: [{
+      test: /\.jsx?$/,
+      exclude: /(node_modules|bower_components)/,
+      loader: 'babel',
+      query: {
+        presets: ['react', 'es2015', 'stage-0'],
+      },
+    }],
+  },
+  plugins: [],
+};
