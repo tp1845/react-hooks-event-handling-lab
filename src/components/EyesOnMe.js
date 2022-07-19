51047a7da9425a54dee019f067214dc601324397
manyshyn <manyshynyuri@gmail.com>
2022-07-19 08:32:55 -0500

    done

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,20 @@
 // Code EyesOnMe Component Here
+import React from 'react'
+
+function EyesOnMe(){
+
+    function handleFocus(){
+        console.log("Good!")
+    }
+
+    function handleBlur() {
+        console.log("Hey! Eyes on me!");
+      }
+    return (
+        <div>
+            <button onFocus={handleFocus} onBlur={handleBlur}>Eyes on me</button>
+        </div>
+    )
+}
+
+export default EyesOnMe;
--- src/components/Keypad.js
+++ src/components/Keypad.js
@@ -1,1 +1,14 @@
-// Code Keypad Component Here
+import React from 'react'
+
+function Keypad() {
+
+  function handleChange() {
+    console.log("Entering password...")
+  }
+
+  return (
+    <input type="password" onChange={handleChange} />
+  )
+}
+
+export default Keypad
